d276831bacb4db9c2edd6e3cdb210c047953f813
Luisa Fernanda Velasquez <101362881+nanditavelasquez@users.noreply.github.com>
2023-07-18 16:01:50 -0500

    Update validar.php

--- validar.php
+++ validar.php
@@ -1,5 +1,48 @@
 <html>
      <body>
-         
+         <?php
+   
+          // Obtengo los datos cargados en el formulario de registro.
+          $usuario = $_POST['usuario'];
+          $pass = $_POST['contraseña'];
+
+          // Guardo en la sesión el email del usuario.
+          $_SESSION['usuario'] = $usuario;
+
+          // Crear conexión con la base de datos.
+          $conexion = mysqli_connect("localhost","root","","cerveceria");
+
+          $consulta = "SELECT * FROM usuario WHERE usuario = '$usuario' AND clave = '$pass' ";
+          $resultado = mysqli_query($conexion,$consulta);
+
+          $filas = mysqli_num_rows($resultado);
+
+          // Validar la conexión de base de datos.
+          if($filas)
+          {
+            header("location: index.php");
+          }
+
+          else
+          {
+            ?>
+              <?php
+                include("login.html");
+            ?>
+            <script>
+                  Swal.fire({
+                  title: 'Mensaje de alerta',
+                  text: 'contraseña o usuario incorrecto',
+                  icon: 'error',
+                 });
+
+                </script>
+            <?php
+                }
+
+             mysqli_free_result($resultado);
+             mysqli_close($conexion);
+            ?>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </body>
 </html>
